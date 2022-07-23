10d2b659c2411f4d856e02056aeedc0824a04e55
Weed <phamhuyhoang3004@gmail.com>
2022-07-23 11:29:39 +0700

    add controller

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,11 @@ dependencies {
     implementation("eu.vendeli:telegram-bot:1.5.2")
     implementation("org.slf4j:slf4j-api:2.0.0-alpha1")
     implementation("org.slf4j:slf4j-simple:2.0.0-alpha1")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
+    implementation(group = "org.redisson", name = "redisson", version = "3.17.0") {
+        exclude("com.fasterxml.jackson.core", "jackson-databind")
+    }
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
 }
 
 tasks.test {
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,24 @@
+
 import eu.vendeli.tgbot.TelegramBot
+import eu.vendeli.tgbot.annotations.TelegramCommand
+import eu.vendeli.tgbot.annotations.TelegramInput
 import eu.vendeli.tgbot.api.message
+import eu.vendeli.tgbot.types.User
+import eu.vendeli.tgbot.types.internal.ProcessedUpdate
+import eu.vendeli.tgbot.types.internal.UpdateType
 
 suspend fun main() {
-    val bot = TelegramBot("5425795377:AAFpD2STi6lQdlieBS8z9xPAIh3w-QNvdLk")
+    val BOT_TOKEN : String = "5425795377:AAFpD2STi6lQdlieBS8z9xPAIh3w-QNvdLk"
+    val bot = TelegramBot(BOT_TOKEN, "org.example")
+    /**
+     * Second parameter is the package in which commands/inputs will be searched.
+     */
 
-    bot.handleUpdates { update ->
-        onCommand("/start") {
-            message { "Ni hao" }.send(update.message!!.from!!.id, bot)
-        }
-        onCommand("/bye") {
-            message { "bye bye" }.send(update.message!!.from!!.id, bot)
-        }
-    }
+    bot.handleUpdates()
+    // start long-polling listener
+}
+
+@TelegramCommand(["/start"])
+suspend fun start(user: User, bot: TelegramBot) {
+    message { "Hello" }.send(to = user, via = bot)
 }
